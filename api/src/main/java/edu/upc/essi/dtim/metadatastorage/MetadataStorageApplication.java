2cc0ff1e5443ba3e3a66de4eca86c94f8403d401
Javier Flores <javierfh1404@gmail.com>
2021-07-11 03:43:39 +0200

    fdsa

--- api/src/main/java/edu/upc/essi/dtim/metadatastorage/MetadataStorageApplication.java
+++ api/src/main/java/edu/upc/essi/dtim/metadatastorage/MetadataStorageApplication.java
@@ -8,9 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MetadataStorageApplication {
 
-	private static final Logger LOGGER =
-			LoggerFactory.getLogger(MetadataStorageApplication.class);
-
 	public static void main(String[] args) {
 		SpringApplication.run(MetadataStorageApplication.class, args);
 	}
