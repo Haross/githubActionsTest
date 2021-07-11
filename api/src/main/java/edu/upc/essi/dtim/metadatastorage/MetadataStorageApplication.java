bd7ab07550c20dc29ca0a563a8ce662f4a69714c
Javier Flores <javierfh1404@gmail.com>
2021-07-11 03:47:49 +0200

    prueba

--- api/src/main/java/edu/upc/essi/dtim/metadatastorage/MetadataStorageApplication.java
+++ api/src/main/java/edu/upc/essi/dtim/metadatastorage/MetadataStorageApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MetadataStorageApplication {
 
-//hola
+//holads
 	public static void main(String[] args) {
 		SpringApplication.run(MetadataStorageApplication.class, args);
 	}
